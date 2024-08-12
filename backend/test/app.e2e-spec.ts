1741d5e1fa1cff49f6311a2c45ad35fc7ff82fe7
hamilgdev <hamilgdev@gmail.com>
2024-08-12 14:09:21 -0500

    chore: Remove unused test files for users controller and service

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,24 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '@/src/app.module';
+import { PrismaService } from '@/src/prisma/prisma.service';
 
-describe('AppController (e2e)', () => {
+describe('HealthyController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue({})
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/api/healthy (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/healthy')
       .expect(200)
-      .expect('Hello World!');
+      .expect({ ok: true });
   });
 });
